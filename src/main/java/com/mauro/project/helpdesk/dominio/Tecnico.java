64753335a0c1f7e71d9b2bfadda76046a36250be
Mauro-vidal <mauroviidal@gmail.com>
2023-09-20 18:14:04 -0300

    Atributos das classes criados

--- src/main/java/com/mauro/project/helpdesk/dominio/Tecnico.java
+++ src/main/java/com/mauro/project/helpdesk/dominio/Tecnico.java
@@ -5,16 +5,16 @@ import com.mauro.project.helpdesk.domain.enums.Perfil;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Cliente extends Pessoa{
+public class Tecnico extends Pessoa {
 
     private List<Chamado> chamados = new ArrayList<>();
 
-    public Cliente(){
+    public Tecnico(){
         super();
         addPerfil(Perfil.CLIENTE);
     }
 
-    public Cliente(Integer id, String nome, String cpf, String email, String senha) {
+    public Tecnico(Integer id, String nome, String cpf, String email, String senha){
         super(id, nome, cpf, email, senha);
         addPerfil(Perfil.CLIENTE);
     }
