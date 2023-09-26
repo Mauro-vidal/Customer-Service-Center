a96af21dcdbe94b94532ee1b4ecd3719c15f5627
Mauro-vidal <mauroviidal@gmail.com>
2023-09-26 16:40:33 -0300

    findAll para Chamado.

--- src/main/java/com/mauro/project/helpdesk/resources/ChamadoResource.java
+++ src/main/java/com/mauro/project/helpdesk/resources/ChamadoResource.java
@@ -3,6 +3,7 @@ package com.mauro.project.helpdesk.resources;
 
 import com.mauro.project.helpdesk.dominio.Chamado;
 import com.mauro.project.helpdesk.dtos.ChamadoDTO;
+import com.mauro.project.helpdesk.dtos.ClienteDTO;
 import com.mauro.project.helpdesk.services.ChamadoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,6 +11,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/chamados")
@@ -22,8 +26,13 @@ public class ChamadoResource {
     public ResponseEntity<ChamadoDTO> findById(@PathVariable Integer id){
         Chamado obj = service.findById(id);
         return ResponseEntity.ok().body(new ChamadoDTO(obj));
-
     }
 
+    @GetMapping
+    public ResponseEntity<List<ChamadoDTO>> findAll(){
+        List<Chamado> list = service.findAll();
+        List<ChamadoDTO> listDto = list.stream().map(obj -> new ChamadoDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
 
 }
