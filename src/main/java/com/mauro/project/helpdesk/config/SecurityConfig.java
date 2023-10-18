147a62a8a1c071952d45c1e2a8906fc9052b9d66
Mauro-vidal <mauroviidal@gmail.com>
2023-10-17 23:12:27 -0300

    Autorização com JWT.

--- src/main/java/com/mauro/project/helpdesk/config/SecurityConfig.java
+++ src/main/java/com/mauro/project/helpdesk/config/SecurityConfig.java
@@ -2,17 +2,20 @@ package com.mauro.project.helpdesk.config;
 
 
 import com.mauro.project.helpdesk.security.JWTAuthenticationFilter;
+import com.mauro.project.helpdesk.security.JWTAuthorizationFilter;
 import com.mauro.project.helpdesk.security.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -20,6 +23,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import java.util.Arrays;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private static final String[] PUBLIC_MATCHERS = {"/h2-console/**"};
@@ -42,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
         http.cors().and().csrf().disable();
         http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+        http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
         http.authorizeRequests()
                 .antMatchers(PUBLIC_MATCHERS).permitAll()
                 .anyRequest().authenticated();
--- src/main/java/com/mauro/project/helpdesk/security/JWTUtil.java
+++ src/main/java/com/mauro/project/helpdesk/security/JWTUtil.java
@@ -2,6 +2,7 @@ package com.mauro.project.helpdesk.security;
 
 import java.util.Date;
 
+import io.jsonwebtoken.Claims;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -25,4 +26,33 @@ public class JWTUtil {
     }
 
 
+    public boolean tokenValido(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null){
+            String username = claims.getSubject();
+            Date expirationDate = claims.getExpiration();
+            Date now = new Date(System.currentTimeMillis());
+
+            if (username != null && expirationDate != null && now.before(expirationDate)){ // verificando se o token já expirou ou não
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Claims getClaims(String token) {
+        try {
+            return Jwts.parser().setSigningKey(secret.getBytes()).parseClaimsJwt(token).getBody();
+        } catch (Exception e){
+            return null;
+        }
+    }
+
+    public String getUsername(String token) {
+        Claims claims = getClaims(token);
+        if (claims != null){
+            return claims.getSubject();
+        }
+        return null;
+    }
 }
